7cab2ea9a15ca2645160aa77c83d1b9fd1282726
qw3r-T <qqw33r.T@gmail.com>
2022-07-11 10:35:20 +0200

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray) {
+	for (let i = 0; i <= (arguments.length-1); i++) {
+		let index = inputArray.indexOf(arguments[i+1]);
+		if (index > -1) {inputArray.splice(index,1)};
+	};
+	return inputArray;
 };
 
 // Do not edit below this line
