94fb9449a4e77ee7a5cc3cf016e9e89a58f3cfb3
qw3r-T <qqw33r.T@gmail.com>
2022-07-11 10:48:53 +0200

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+	if (typeof min != 'number' || typeof max != 'number'
+		|| min < 0 || max < 0){return 'ERROR'};
+	let result = 0;
+	if (min > max) {let tmp = max; max = min; min = tmp; };
+	for (min;min <= max;min++) {result += min};
+	return result;
 };
 
 // Do not edit below this line
