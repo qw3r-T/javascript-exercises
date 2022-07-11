00e2bd591477e1e4da4f1c9abcf55b7320f3e8ae
qw3r-T <qqw33r.T@gmail.com>
2022-07-11 10:11:13 +0200

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+	let inputArray = inputString.slice('');
+	let outputArray = [];
+	for (let i = 0; i < inputArray.length; i++) {
+		outputArray.push(inputArray[inputArray.length-(i+1)]);
+	};
+	let outputString = outputArray.join('')
+	return outputString;
 };
 
 // Do not edit below this line
