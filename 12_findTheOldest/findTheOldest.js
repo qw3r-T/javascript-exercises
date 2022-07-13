13287d93dae4ea0eef77a75690a3ee6039ef1e45
qw3r-T <qqw33r.T@gmail.com>
2022-07-13 15:56:12 +0200

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+	
+	return array.reduce((oldest, current) => {
+		const oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+		const currentAge = getAge(current.yearOfBirth,current.yearOfDeath);
+		return oldestAge > currentAge ? oldest : current;
+	})
 
+	function getAge(birth,death) {
+		if (!death) {
+			death = new Date().getFullYear();
+		};
+		return death - birth;
+	};
 };
 
 // Do not edit below this line
