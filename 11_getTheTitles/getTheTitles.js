8dad0269540f52b63dd32de3efe9aaf8449f474c
qw3r-T <qqw33r.T@gmail.com>
2022-07-13 15:43:31 +0200

    exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(list) {
+	//create returnList
+	let returnList = [];
+	//loop through the list
+	list.forEach((e)=>returnList.push(e.title));
+	//return the filled list
+	return returnList;
 };
 
 // Do not edit below this line
