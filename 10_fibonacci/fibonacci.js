6ad4e1ca35e67e9d78f3169c96535825650baccf
qw3r-T <qqw33r.T@gmail.com>
2022-07-13 13:59:37 +0200

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
-};
-
+function fibonacci(a) {
+	if (a < 0) {return 'OOPS';};
+	let n1 = 0, n2 = 1, nextTerm;
+	for (let i = 0; i < a; i++) {
+		nextTerm = n1 + n2;
+		n1 = n2;
+		n2 = nextTerm;
+	};
+	return n1;
+}
 // Do not edit below this line
 module.exports = fibonacci;
