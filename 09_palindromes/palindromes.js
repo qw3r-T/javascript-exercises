c66ebbed1b53d76b67adf76ac03253f7b8c0b104
qw3r-T <qqw33r.T@gmail.com>
2022-07-13 13:43:56 +0200

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+	//remove space and punctuation
+	text = text.replace(/[^a-z]/gi, '');
+	console.log(text);
+	//lowercase
+	text = text.toLowerCase()
+	//check if it's a palindrome
+	for (let i = 0; i<text.length; i++) {
+		if (text[i] != text[text.length-(i+1)]) {return false};
+	};
+	return true;
 };
 
 // Do not edit below this line
