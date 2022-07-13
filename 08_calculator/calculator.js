324da0cb59bf72e156a2823bb1cc063a5695b1a1
qw3r-T <qqw33r.T@gmail.com>
2022-07-13 12:30:57 +0200

    exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
 const add = function() {
-	
+	let total = 0;
+	for (let i = 0; i<arguments.length; i++) {
+		total += arguments[i];
+	};
+	return total;
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];	
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+	let total = 0;
+	for (let i = 0; i<list.length; i++) {
+		total += list[i];
+	};
+	return +total;
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+	let total = 1;
+	for (let i = 0; i<list.length; i++) {
+		total *= list[i];
+	};
+	return +total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+	for (a; a > 0; a--) {
+		total *= a;
+	};
+	return total;
 };
 
 // Do not edit below this line
